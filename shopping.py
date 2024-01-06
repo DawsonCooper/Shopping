85047f47b6fa93e9cb145014937b9018a9e0d036
DawsonCooper <dawsoncooper96@gmail.com>
2024-01-06 08:09:15 -0600

    Data loaded and formatted

--- shopping.py
+++ shopping.py
@@ -114,6 +114,7 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
+    
     raise NotImplementedError
 
 
